7527b132a645dd6ea4f9aa2998f6bd2e31890ad4
Sten Roger Sandvik <srs@enonic.com>
2017-04-04 23:07:17 +0200

    Implement tests (fixes #9)

--- tests.ts
+++ tests.ts
@@ -1,5 +1,66 @@
 {
-  bitbot.neo();
-  bitbot.motor(bitbot.Motor.All, 1023);
-}
+  // Drive 100 ms forward
+  bitbot.motor(BBMotor.All, 1023);
+  basic.pause(100);
 
+  // Drive 100 ms reverse
+  bitbot.motor(BBMotor.All, -1023);
+  basic.pause(100);
+
+  // Drive 100 ms forward on left and reverse on right
+  bitbot.motor(BBMotor.Left, 1023);
+  bitbot.motor(BBMotor.Right, -1023);
+  basic.pause(100);
+
+  // Buzz for 100 ms
+  bitbot.buzz(1);
+  basic.pause(100);
+  bitbot.buzz(0);
+
+  // Read left and right line sensor
+  basic.showNumber(bitbot.readLine(BBLineSensor.Left));
+  basic.showNumber(bitbot.readLine(BBLineSensor.Right));
+
+  // Read left and right light sensor
+  basic.showNumber(bitbot.readLight(BBLightSensor.Left));
+  basic.showNumber(bitbot.readLight(BBLightSensor.Right));
+
+  // Read sonar values
+  basic.showNumber(bitbot.sonar(BBPingUnit.MicroSeconds));
+  basic.showNumber(bitbot.sonar(BBPingUnit.Centimeters));
+  basic.showNumber(bitbot.sonar(BBPingUnit.Inches));
+
+  // Show all leds
+  bitbot.neoShowColor(neopixel.colors(NeoPixelColors.Red));
+  bitbot.neoShow();
+
+  // Clear all leds
+  bitbot.neoClear();
+  bitbot.neoShow();
+
+  // Show led at position 1
+  bitbot.neoSetPixelColor(0, neopixel.colors(NeoPixelColors.Red));
+  bitbot.neoShow();
+
+  // Show led rainbow
+  bitbot.neoShowRainbow();
+  bitbot.neoShow();
+
+  // Show led rainbow and shift
+  bitbot.neoShowRainbow();
+  bitbot.neoShift();
+  bitbot.neoShow();
+
+  // Show led rainbow and rotate
+  bitbot.neoShowRainbow();
+  bitbot.neoRotate();
+  bitbot.neoShow();
+
+  // Set brightness of leds
+  bitbot.neoBrightness(100);
+  bitbot.neoShow();
+
+  // Use neo() variable
+  bitbot.neo().clear();
+  bitbot.neo().show();
+}
